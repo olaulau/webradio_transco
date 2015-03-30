dd70317ce4ec23b0680c0f2a9939cc67115276ad
olaulau <olaulau@gmail.com>
2015-03-30 05:01:38 +0200

    php proxify the stream start and stop controled by PHP waiting HTTP ready in PHP -> works perfectly with 1 user

---
+++ stream.php
@@ -0,0 +1,35 @@
+<?php
+
+// register shutdown callback, to stop the stream server
+function shutdown()
+{
+	exec('./stop.sh');
+}
+register_shutdown_function('shutdown');
+
+
+// start the stream server
+exec('./start.sh');
+
+// wait for the HTTP server to respond
+function test_http() {
+	$connection = @fsockopen("localhost", 8000);
+	return is_resource($connection);
+}
+while(!test_http()) {
+	usleep(100*1000); // 0.1s 
+}
+
+
+
+// start streaming
+$handle = fopen("http://localhost:8000/", "rb");
+if (FALSE === $handle) {
+	exit("Echec lors de l'ouverture du flux vers l'URL");
+}
+
+while (!feof($handle)) {
+	$buffer = fread($handle, 8192);
+	echo $buffer;
+}
+fclose($handle);
