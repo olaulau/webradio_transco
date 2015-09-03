5e95a6faf7c0879064d76dbb9968ba50e653337e
olaulau <olaulau@gmail.com>
2015-09-03 19:11:53 +0200

    bigger glyphicon

--- index.php
+++ index.php
@@ -21,6 +21,8 @@ Stream::create_structure ();
 <link href="external/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <link href="external/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
 
+<link href="index.css" rel="stylesheet">
+
 
 
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -126,7 +128,7 @@ foreach ( $streams as $s ) {
 		}
 		?>
 		<td><?=$s->get_mux() . ' ' . $s->get_acodec() . ' ' . $s->get_ab() . ' kbps'?></td>
-		<td><a href="stream.php?id=<?= $s->get_id()?>">GO</a></td>
+		<td><a href="stream.php?id=<?= $s->get_id()?>"><span class="glyphicon glyphicon-play" aria-hidden="true" title="play"></span></a></td>
 		<?php
 		if(Admin::is_admin()) {
 		?>
